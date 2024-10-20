bf828be1cefb552c20dbce3d71b324469713f3f0
bgno1 <33606683@qq.com>
2024-10-21 00:18:44 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# yolov8_plant_detection
+# Optimizing YOLOv8 for Lightweight and Accurate UAV-based Plant Detection
+
+
+
+
+
+This repository contains the code supporting the research for the paper **"Optimizing YOLOv8 for Lightweight and Accurate UAV-based Plant Detection"** (currently under peer review). The modifications and experiments in this work are aimed at enhancing the YOLOv8 model to improve its performance for UAV-based plant detection tasks, balancing both accuracy and computational efficiency.
+
+
+
+The repository includes:
+
+- **`config`**: This folder contains the configuration files for the models used in the experiments described in the paper. Each file corresponds to a specific model setup and can be used to replicate the results.
+
+
+
+In this study, we replaced the original CIoU loss function in YOLOv8 with the WIoU loss function to enhance the accuracy of bounding box regression for small plant targets. To implement WIoU in YOLOv8, follow the instructions below.
+
+- Import `wiou_loss` (the `wiou_loss.py` file can be found in the `yolo` directory of this repository) into `ultralytics/utils/loss.py`.
+
+- Modify the `forward` function in the `BboxLoss` class in `ultralytics/utils/loss.py` by adding the following code:
+  
+  ```python
+  if config.bbox_loss == 'WIoU':
+      loss, iou = bbox_wiou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False)
+      loss_iou = (loss * weight).sum() / target_scores_sum
+  else:   # CIoU
+      iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
+      loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
+  ```
---
+++ wiou_loss.py
@@ -0,0 +1,124 @@
+import torch
+from ultralytics import config
+
+class IoU_Cal:
+    ''' pred, target: x0,y0,x1,y1
+        monotonous: {
+            None: origin
+            True: monotonic FM
+            False: non-monotonic FM
+        }
+        momentum: The momentum of running mean'''
+    iou_mean = 1.
+    momentum = 1 - pow(0.5, exp=1 / 7000)
+    _is_train = True
+
+    def __init__(self, pred, target, monotonous=False):
+        self.pred, self.target = pred, target
+        self.monotonous = monotonous
+        self._fget = {
+            # x,y,w,h
+            'pred_xy': lambda: (self.pred[..., :2] + self.pred[..., 2: 4]) / 2,
+            'pred_wh': lambda: self.pred[..., 2: 4] - self.pred[..., :2],
+            'target_xy': lambda: (self.target[..., :2] + self.target[..., 2: 4]) / 2,
+            'target_wh': lambda: self.target[..., 2: 4] - self.target[..., :2],
+            # x0,y0,x1,y1
+            'min_coord': lambda: torch.minimum(self.pred[..., :4], self.target[..., :4]),
+            'max_coord': lambda: torch.maximum(self.pred[..., :4], self.target[..., :4]),
+            # The overlapping region
+            'wh_inter': lambda: self.min_coord[..., 2: 4] - self.max_coord[..., :2],
+            's_inter': lambda: torch.prod(torch.relu(self.wh_inter), dim=-1),
+            # The area covered
+            's_union': lambda: torch.prod(self.pred_wh, dim=-1) +
+                               torch.prod(self.target_wh, dim=-1) - self.s_inter,
+            # The smallest enclosing box
+            'wh_box': lambda: self.max_coord[..., 2: 4] - self.min_coord[..., :2],
+            's_box': lambda: torch.prod(self.wh_box, dim=-1),
+            'l2_box': lambda: torch.square(self.wh_box).sum(dim=-1),
+            # The central points' connection of the bounding boxes
+            'd_center': lambda: self.pred_xy - self.target_xy,
+            'l2_center': lambda: torch.square(self.d_center).sum(dim=-1),
+            # IoU
+            'iou': lambda: 1 - self.s_inter / self.s_union
+        }
+        self._update(self)
+
+    def __setitem__(self, key, value):
+        self._fget[key] = value
+
+    def __getattr__(self, item):
+        if callable(self._fget[item]):
+            self._fget[item] = self._fget[item]()
+        return self._fget[item]
+
+    @classmethod
+    def train(cls):
+        cls._is_train = True
+
+    @classmethod
+    def eval(cls):
+        cls._is_train = False
+
+    @classmethod
+    def _update(cls, self):
+        if cls._is_train: cls.iou_mean = (1 - cls.momentum) * cls.iou_mean + \
+                                         cls.momentum * self.iou.detach().mean().item()
+
+    def _scaled_loss(self, loss, gamma=1.9, delta=3):
+        if isinstance(self.monotonous, bool):
+            if self.monotonous:
+                loss *= (self.iou.detach() / self.iou_mean).sqrt()
+            else:
+                beta = self.iou.detach() / self.iou_mean
+                alpha = delta * torch.pow(gamma, beta - delta)
+                loss *= beta / alpha
+        return loss
+
+    @classmethod
+    def IoU(cls, pred, target, self=None):
+        self = self if self else cls(pred, target)
+        return self.iou
+
+    @classmethod
+    def WIoU(cls, pred, target, self=None):
+        self = self if self else cls(pred, target)
+        dist = torch.exp(self.l2_center / self.l2_box.detach())
+        return self._scaled_loss(dist * self.iou)
+
+
+def bbox_wiou(box1, box2, xywh=False, eps=1e-7):
+
+    if xywh:  # transform from xywh to xyxy
+        (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, -1), box2.chunk(4, -1)
+        w1_, h1_, w2_, h2_ = w1 / 2, h1 / 2, w2 / 2, h2 / 2
+        b1_x1, b1_x2, b1_y1, b1_y2 = x1 - w1_, x1 + w1_, y1 - h1_, y1 + h1_
+        b2_x1, b2_x2, b2_y1, b2_y2 = x2 - w2_, x2 + w2_, y2 - h2_, y2 + h2_
+    else:  # x1, y1, x2, y2 = box1
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1.chunk(4, -1)
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2.chunk(4, -1)
+        w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+        w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+
+    # Intersection area
+    inter = (b1_x2.minimum(b2_x2) - b1_x1.maximum(b2_x1)).clamp_(0) * (
+            b1_y2.minimum(b2_y2) - b1_y1.maximum(b2_y1)
+    ).clamp_(0)
+
+    # Union Area
+    union = w1 * h1 + w2 * h2 - inter + eps
+
+    # IoU
+    iou = inter / union
+
+    cw = b1_x2.maximum(b2_x2) - b1_x1.minimum(b2_x1)  # convex (smallest enclosing box) width
+    ch = b1_y2.maximum(b2_y2) - b1_y1.minimum(b2_y1)  # convex height
+
+
+
+    b1 = torch.stack([b1_x1, b1_y1, b1_x2, b1_y2], dim=-1)
+    b2 = torch.stack([b2_x1, b2_y1, b2_x2, b2_y2], dim=-1)
+
+    self = IoU_Cal(b1, b2, monotonous=False)
+    loss = getattr(IoU_Cal, 'WIoU')(b1, b2, self=self)
+    iou = 1 - self.iou
+    return loss, iou
